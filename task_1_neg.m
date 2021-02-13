77ae4e567ce7a681840f4a6a9ba19dba37d021fd
NadiraMason <60779278+NadiraMason@users.noreply.github.com>
2021-02-13 13:32:47 +0000

    Add files via upload

--- task_1_neg.m
+++ task_1_neg.m
@@ -5,7 +5,7 @@ neg_stim.Properties.VariableNames{'Var2'}='Frame';
 neg_stim.Properties.VariableNames{'Var3'}='x';
 neg_stim.Properties.VariableNames{'Var4'}='y';
 
-%defining matrices for the velocities and accelerations
+%defining matrices for the positions, velocities and accelerations
 pos_mat = [];
 vel_mat = [];
 acc_mat = [];
@@ -14,11 +14,12 @@ acc_mat = [];
 for n = 0:49
     
     fish_pos = [neg_stim.x((50*n)+(n+1):(50*n)+(n+50)) neg_stim.y((50*n)+(n+1):(50*n)+(n+50))]; %defines position vector
-    pos_mat = [pos_mat fish_pos];
+    pos_mat = [pos_mat fish_pos]; %adds position vector to matrix of fish position vectors
     
-    %defines the initial velocities and accelerations
-    fish_vel = [0 0]; 
-    fish_acc = [0 0];
+    %defines the initial velocities and accelerations that we can't obtain a
+    % value for as 'NaN'
+    fish_vel = [NaN NaN]; 
+    fish_acc = [NaN NaN; NaN NaN];
     
     %loops through each position vector, defining its velocity
     for m = 2:50
@@ -27,7 +28,7 @@ for n = 0:49
     end
     
     %loops through each velocity vector, defining its acceleration
-    for r = 2:50
+    for r = 3:50
         acc = fish_vel(r,:) - fish_vel(r-1,:);
         fish_acc = [fish_acc ; acc];
     end
@@ -37,24 +38,29 @@ for n = 0:49
     acc_mat = [acc_mat fish_acc];
 end
 
-acc_min_x = min(min(mean(acc_mat(:,1:2:end),2)));
-[x,y] = find(mean(acc_mat(:,1:2:end),2) == acc_min_x);
+%plots the position vector of each fish for every frame number 
+for i = 1:50
+    figure(1);
+    plot(pos_mat(:,2*i-1),pos_mat(:,2*i),'o')
+    title('The position vectors of each fish over time');
+    xlabel('x coordinate');
+    ylabel('y coordinate');
+    hold on
+end
+hold off
 
-acc_min_y = min(min(mean(acc_mat(:,2:2:end),2)));
-[p,q] = find(mean(acc_mat(:,2:2:end),2) == acc_min_y);
-
-mean(pos_mat(x,1:2:end),2)
-mean(pos_mat(p,2:2:end),2)
-
-
-
-mean(acc_mat(:,1:2:end),2);
-
-
-% maximum = max(max(A)); 
-% [x,y]=find(A==maximum)
-
-%plot(neg_stim.Frame(1:50),mean(vel_mat(:,1:2:end),2),neg_stim.Frame(1:50),mean(vel_mat(:,2:2:end),2));
-plot(neg_stim.Frame(1:50),mean(acc_mat(:,1:2:end),2),neg_stim.Frame(1:50),mean(acc_mat(:,2:2:end),2));
-
+%plot the mean velocity of the fish against the frame number
+figure(2);
+plot(neg_stim.Frame(1:50),mean(vel_mat(:,1:2:end),2),neg_stim.Frame(1:50),mean(vel_mat(:,2:2:end),2));
 legend('x','y');
+title('Mean velocity of the fish over time');
+xlabel('Frame Number');
+ylabel('Mean Velocity');
+
+%plot the mean acceleration of the fish against the frame number
+figure(3);
+plot(neg_stim.Frame(1:50),mean(acc_mat(:,1:2:end),2),neg_stim.Frame(1:50),mean(acc_mat(:,2:2:end),2));
+legend('x','y');
+title('Mean acceleration of the fish over time');
+xlabel('Frame Number');
+ylabel('Mean Acceleration');
